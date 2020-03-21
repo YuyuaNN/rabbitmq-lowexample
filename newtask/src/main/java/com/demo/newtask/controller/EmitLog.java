b933bbc36a65ebad05d48c7eb18e7da9f9802a31
yuyuan <1455578041@qq.com>
2020-03-21 17:53:43 +0800

    rabittmq实例 EmitLog https://www.rabbitmq.com/tutorials/tutorial-three-java.html

---
+++ newtask/src/main/java/com/demo/newtask/controller/EmitLog.java
@@ -0,0 +1,33 @@
+package com.demo.newtask.controller;
+
+import com.demo.newtask.NewTaskApplication;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmitLog {
+
+    private static final String EXCHANGE_NAME = "logs";
+
+    @RequestMapping("/EmitLog")
+    String index() throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
+
+            String[] argv = NewTaskApplication.str;
+            String message = argv.length < 1 ? "info: Hello World!" :
+                    String.join(" ", argv);
+
+            channel.basicPublish(EXCHANGE_NAME, "", null, message.getBytes("UTF-8"));
+            System.out.println(" [x] Sent '" + message + "'");
+        }
+
+        return "EmitLog ! ";
+    }
+}
---
+++ work/src/main/java/com/demo/work/controller/ReceiveLogs.java
@@ -0,0 +1,35 @@
+package com.demo.work.controller;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReceiveLogs {
+    private static final String EXCHANGE_NAME = "logs";
+
+    @RequestMapping("/ReceiveLogs")
+    public String index() throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
+        String queueName = channel.queueDeclare().getQueue();
+        channel.queueBind(queueName, EXCHANGE_NAME, "");
+
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println(" [x] Received '" + message + "'");
+        };
+        channel.basicConsume(queueName, true, deliverCallback, consumerTag -> { });
+
+        return "ReceiveLogs2 !";
+    }
+}
